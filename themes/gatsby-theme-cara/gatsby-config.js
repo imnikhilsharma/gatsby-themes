3b1b72d65d2e5361e11dddd12443afb1da45b5ff
Nikhil Sharma <50795450+imnikhilsharma@users.noreply.github.com>
2020-11-27 22:55:56 +0530

    Update gatsby-config.js

--- themes/gatsby-theme-cara/gatsby-config.js
+++ themes/gatsby-theme-cara/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = ({ basePath = `/`, mdx = true }) => ({
     siteTitleAlt: `Cara - @lekoarts/gatsby-theme-cara`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `Looking for Tachnical SEO Auditor that help to boost your presence on Google Search Engine. Get In touch with me at:+91-9588066361`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
